9ea1f351e12daf232a7a6efbc9de8489aa6ed8ee
Emma Ledbetter <e.lledbetter@gmail.com>
2023-08-04 18:37:20 -0500

    implement GUI

--- graph.py
+++ graph.py
@@ -1,27 +1,17 @@
-'''
-will be used to display number of comparisons each algorithm makes
-for now just testing stuff
-
-TODO: implement way to track and display comparisons each algorithm makes
-'''
-
 import matplotlib.pyplot as plt
 from sort import *
 
-'''
-get # of comparisons from each algorithm
-    
-display graph
-'''
-sortAlgorithms = ["bubblesort", "mergesort", "selectionsort"]
+algorithmList = ["bubblesort", "mergesort", "selectionsort"]
 
-def drawGraph(comparisons):
-    global sortAlgorithms
+# display data in a graph
+def drawGraph(comparisonsList):
+    global algorithmList
 
-    plt.bar(sortAlgorithms, comparisons)
+    plt.bar(algorithmList, comparisonsList)
     plt.ylabel('comparisons')
     plt.show()
 
+# get comparisons each algorithm makes
 def compareSort(inputList):
     # is there a way to iterate through these?
     bubblesort.sort(inputList)
@@ -29,10 +19,7 @@ def compareSort(inputList):
     selectionsort.sort(inputList)
 
     comparisons = [bubblesort.comparisons, 
-              mergesort.comparisons, 
-              selectionsort.comparisons]
+                   mergesort.comparisons, 
+                   selectionsort.comparisons]
     
     drawGraph(comparisons)
-
-unsorted = [9, 2, 3, 1, 19, 0]
-compareSort(unsorted)
--- gui.py
+++ gui.py
@@ -3,3 +3,33 @@ UI for user to input settings for the list each algorithm will sort
 
 TODO: get user input and pass it to graph.py
 '''
+import tkinter as tk
+import random
+import graph
+
+# create randomized list of inputted length and pass it to graph module
+def createRandomizedList(numElements):
+    unsorted = []
+
+    for a in range(0, numElements):
+        unsorted.append(random.randrange(0, 100))
+    print("Sorting list with", numElements, "random elements")
+    graph.compareSort(unsorted)
+
+# create main input GUI
+def createGUI():
+    mainWindow = tk.Tk()
+    mainWindow.title("title")
+    mainWindow.geometry('300x200')
+
+    tk.Label(mainWindow, text="List size").grid(row=0)
+    sizeInput = tk.Entry(mainWindow)
+    sizeInput.grid(row=0, column=1)
+    
+    # TODO: get user input
+    submitButton = tk.Button(mainWindow, text="Sort!", command=lambda: createRandomizedList(5)).grid(row=1)
+
+    mainWindow.mainloop()
+
+if __name__ == "__main__":
+    createGUI()
